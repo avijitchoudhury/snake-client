3192e7bbf83567437a3c8042948455d2b9df46cb
avijitchoudhury <avijit.choudhury24@gmail.com>
2019-09-25 18:00:58 +0000

    creating two functiosn within play.js as handleUserInput

--- client.js
+++ client.js
@@ -5,11 +5,21 @@ const connect = function () {
   // interpret incoming data as text
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+    conn.on('connect', () => {
+      // conn.on('connect', () => {
+      //   conn.write('Move: up');
+        console.log('Successfully connected to game server')
+        conn.write('Name: Avi');
+      // });
+    });
   });
   conn.setEncoding('utf8');
-
+  
   return conn;
 }
 
+
 module.exports = { connect };
+
+
 
--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-const net = require('net');
+// const net = require('net');
 
 /**
  * Establishes connection with the game server
@@ -9,3 +9,19 @@ connect();
 
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = function (data) {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.handleUserInput(data);
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
